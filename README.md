e11979a49d90acef986014ef3a4891953e509a7b
Mac <mac@LyuKundeMBP.lan>
2023-03-18 20:51:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Github Actions Padavan RM2100
+
+- Padavan源码是[MeIsReallyBa/padavan-4.4](https://github.com/MeIsReallyBa/padavan-4.4)。
+- Github Actions参考自[Ljzkirito/Actions-Padavan_Redmi-AC2100](https://github.com/Ljzkirito/Actions-Padavan_Redmi-AC2100)。
+- 编译目标为Redmi-AC2100
+- 默认登陆地址[192.168.2.1](http://192.168.5.1),登录名admin/admin
+- 开启插件`shadowsocks`,`xray-1.8.0`
+```
+支持xtls-rprx-vison,reality,utls
+注意！此版本xray比较吃内存，开启后剩余内存约为30M
+```
---
+++ genxrayconfig.lua
@@ -0,0 +1,128 @@
+local cjson = require "cjson"
+local server_section = arg[1]
+local proto = arg[2]
+local local_port = arg[3] or "0"
+local socks_port = arg[4] or "0"
+local ssrindext = io.popen("dbus get ssconf_basic_json_" .. server_section)
+local servertmp = ssrindext:read("*all")
+local server = cjson.decode(servertmp)
+local xray = {
+	log = {
+		-- error = "/var/ssrplus.log",
+		loglevel = "warning"
+	},
+	-- 传入连接
+	inbounds = {
+		(local_port ~= "0") and {
+			tag = "all-in",
+			port = local_port,
+			protocol = "dokodemo-door",
+			settings = {
+				network = "tcp,udp",
+				followRedirect = true
+			},
+			sniffing = {
+				enabled = true,
+				destOverride = { "http", "tls" }
+			}
+		} or nil,
+	-- 开启 socks 代理
+		(proto == "tcp" and socks_port ~= "0") and {
+			protocol = "socks",
+			port = socks_port,
+			settings = {
+				auth = "noauth",
+				udp = true
+			},
+			sniffing = {
+				enabled = true,
+				destOverride = { "http", "tls" }
+			}		
+		} or nil,
+	},
+	-- 传出连接
+	outbounds = {
+		{
+			protocol = "vless",
+			settings = {
+				vnext = {
+					{
+						address = server.server,
+						port = tonumber(server.server_port),
+						users = {
+							{
+								id = server.vmess_id,
+								flow = server.flow,
+								level = tonumber(server.alter_id),
+								encryption = server.security
+							}
+						}
+					}
+				}
+			},
+			-- 底层传输配置
+			streamSettings = {
+				network = server.transport,
+				security = (server.tls == '1') and "tls" or ((server.tls == '2') and "reality" or "none"),
+				tlsSettings = (server.tls == '1') and 
+				{
+					show = false,
+					allowInsecure = (server.insecure ~= "0") and true or false,
+					fingerprint = server.tls_fp, serverName=server.tls_host
+				} or nil,
+
+				realitySettings = (server.tls == '2') and
+				{
+					show = false,
+					fingerprint = server.tls_fp,
+					serverName = server.tls_host,
+					publicKey = server.public_key,
+					shortId = server.short_id,
+					spiderX = server.spiderx
+				} or nil,
+
+				kcpSettings = (server.transport == "kcp") and {
+					mtu = tonumber(server.mtu),
+					tti = tonumber(server.tti),
+					uplinkCapacity = tonumber(server.uplink_capacity),
+					downlinkCapacity = tonumber(server.downlink_capacity),
+					congestion = (server.congestion == "1") and true or false,
+					readBufferSize = tonumber(server.read_buffer_size),
+					writeBufferSize = tonumber(server.write_buffer_size),
+					header = {
+						type = server.kcp_guise
+					}
+				} or nil,
+				wsSettings = (server.transport == "ws") and (server.ws_path ~= nil or server.ws_host ~= nil) and {
+					path = server.ws_path,
+					headers = (server.ws_host ~= nil) and {
+						Host = server.ws_host
+					} or nil,
+				} or nil,
+				httpSettings = (server.transport == "h2") and {
+					path = server.h2_path,
+					host = server.h2_host,
+				} or nil,
+				quicSettings = (server.transport == "quic") and {
+					security = server.quic_security,
+					key = server.quic_key,
+					header = {
+						type = server.quic_guise
+					}
+				} or nil
+			},
+			tag = "proxy"
+		},
+		{
+			protocol = "freedom",
+			tag = "direct"
+		},
+		-- 额外传出连接
+		{
+			protocol =  "blackhole",
+			tag = "block"
+		}
+	}
+}
+
+print(cjson.encode(xray))
